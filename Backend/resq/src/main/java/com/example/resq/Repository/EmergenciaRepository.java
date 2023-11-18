5630caf65546f424b3e86a2d2615e43ac6e6f351
Javiera vergara <javiera.vergara.s@usach.cl>
2023-11-18 19:57:44 -0300

    Repositorio emergencia

---
+++ Backend/resq/src/main/java/com/example/resq/Repository/EmergenciaRepository.java
@@ -0,0 +1,13 @@
+package com.example.resq.Repository;
+
+import com.example.resq.Models.Emergencia;
+
+import java.util.List;
+
+public interface EmergenciaRepository {
+    List<Emergencia> getAllEmergencias();
+    Emergencia getEmergenciaById(Integer id);
+    void saveEmergencia(Emergencia emergencia);
+    void deleteEmergencia(Integer id);
+    List<Emergencia> getEmergenciasByEstado(String estado);
+}
---
+++ Backend/resq/src/main/java/com/example/resq/Repository/EmergenciaRepositoryImp.java
@@ -0,0 +1,82 @@
+package com.example.resq.Repository;
+
+import com.example.resq.Models.Emergencia;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@Repository
+public class EmergenciaRepositoryImp implements EmergenciaRepository{
+    @Autowired
+    private Sql2o sql2o;
+
+    @Override
+    public List<Emergencia> getAllEmergencias(){
+        List<Emergencia> salida;
+        System.out.println("Obteniendo las emergencias");
+        String query = "SELECT * FROM Emergencia";
+        try (Connection connection = sql2o.open()){
+            salida = connection.createQuery(query).executeAndFetch(Emergencia.class);
+            return salida;
+        }catch (Exception e){
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    @Override
+    public Emergencia getEmergenciaById(Integer id){
+        String query = "SELECT * FROM Emergencia WHERE id_emergencia = :id";
+        try (Connection connection = sql2o.open()){
+            return connection.createQuery(query)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Emergencia.class);
+        }
+    }
+
+    @Override
+    public void saveEmergencia(Emergencia emergencia){
+        DateTimeFormatter formatoFecha = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate fecha = LocalDate.parse(emergencia.getFecha(), formatoFecha);
+        String query = "INSERT INTO Emergencia (nombre, gravedad, fecha, estado, region, longitud, latitud, id_institucion, geom) " +
+                "VALUES (:nombre, :gravedad, :fecha, :estado, :region, :longitud, :latitud, :id_institucion, ST_PointFromText('POINT(' || :longitud || ' ' || :latitud || ')'))";
+        try (Connection connection = sql2o.open()){
+            connection.createQuery(query)
+                    .addParameter("nombre", emergencia.getNombre())
+                    .addParameter("gravedad", emergencia.getGravedad())
+                    .addParameter("fecha", fecha)
+                    .addParameter("estado", emergencia.getEstado())
+                    .addParameter("region", emergencia.getRegion())
+                    .addParameter("longitud", emergencia.getLongitud())
+                    .addParameter("latitud", emergencia.getLatitud())
+                    .addParameter("id_institucion", emergencia.getId_institucion())
+                    .executeUpdate();
+        }
+    }
+
+    @Override
+    public void deleteEmergencia(Integer id){
+        String query = "DELETE FROM Emergencia WHERE id_emergencia = :id";
+        try (Connection connection = sql2o.open()){
+            connection.createQuery(query)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }
+    }
+
+    @Override
+    public List<Emergencia> getEmergenciasByEstado(String estado){
+        String query = "SELECT * FROM Emergencia WHERE estado = :estado";
+        try (Connection connection = sql2o.open()){
+            return connection.createQuery(query)
+                    .addParameter("estado", estado)
+                    .executeAndFetch(Emergencia.class);
+        }
+    }
+
+}
