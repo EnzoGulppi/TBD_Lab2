3389f542b4d10512eb87663785cee926eecfc8e7
Javiera vergara <javiera.vergara.s@usach.cl>
2023-11-18 23:41:51 -0300

    Faltan los services

---
+++ Backend/resq/src/main/java/com/example/resq/Utils/JwtUtils.java
@@ -0,0 +1,39 @@
+package com.example.resq.Utils;
+
+import com.example.resq.Models.Voluntario;
+import com.example.resq.Repository.VoluntarioEmergenciaRepository;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+    private static String secret = "This_is_secret";
+
+    public String generateJwt(Voluntario voluntario){
+        Date issuedAt = new Date();
+        Claims claims = Jwts.claims()
+                .setIssuer(voluntario.getId().toString())
+                .setIssuedAt(issuedAt);
+        return Jwts.builder().setClaims(claims).compact();
+    }
+
+    public String generateJwt2(String id){
+        Date issuedAt = new Date();
+        Claims claims = Jwts.claims()
+                .setIssuer(id)
+                .setIssuedAt(issuedAt);
+        return Jwts.builder().setClaims(claims).compact();
+    }
+
+    public void verify(String autorization) throws Exception {
+        System.out.println("JwtUtils.java: verify() -> autorization = " + autorization);
+        try{
+            Jwts.parser().parseClaimsJwt(autorization);
+        }catch(Exception e){
+            throw new Exception();
+        }
+    }
+}
