cd82d6ee474cf76eb76c8b83ce1707f2f95dd46d
Marcelo <marcelo.godoy@usach.cl>
2023-11-19 14:30:46 -0300

    Tarea Services

--- Backend/resq/src/main/java/com/example/resq/Services/TareaServices.java
+++ Backend/resq/src/main/java/com/example/resq/Services/TareaServices.java
@@ -1,2 +1,49 @@
-package com.example.resq.Services;public class TareaServices {
+package com.example.resq.Services;
+
+import com.example.resq.Models.Tarea;
+import com.example.resq.Repository.TareaRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tarea")
+public class TareaServices {
+    @Autowired
+    TareaRepository tareaRepository;
+    @GetMapping
+    public List<Tarea> findAllTareas(){
+        return tareaRepository.findAll();
+    }
+    @GetMapping("/{id}")
+    public Tarea findByIdTarea(@PathVariable("id") Integer id){
+        return tareaRepository.findById(id);
+    }
+    @GetMapping("/nombre/{nombre}")
+    public List<Tarea> findByNombreTarea(@PathVariable("nombre") String nombre){
+        return tareaRepository.findByNombre(nombre);
+    }
+    @GetMapping("/emergencia/{id_emergencia}")
+    public List<Tarea> findByEmergenciaTarea(@PathVariable("id_emergencia") Integer id_emergencia){
+        return tareaRepository.findByEmergencia(id_emergencia);
+    }
+    @GetMapping("/region/{region}")
+    public List<Tarea> findByRegionTarea(@PathVariable("region") String region){
+        return tareaRepository.findByRegion(region);
+    }
+    @PostMapping
+    public Tarea saveTarea(@RequestBody Tarea tarea){
+        return tareaRepository.save(tarea);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteTarea(@PathVariable("id") Integer id){
+        tareaRepository.deleteTarea(id);
+    }
+    @PutMapping
+    public Tarea updateTarea(@RequestBody Tarea tarea){
+        return tareaRepository.update(tarea);
+    }
 }
