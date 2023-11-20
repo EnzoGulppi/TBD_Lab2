2af55386ac05d80680ed1b371478fddf22f8c19d
Javiera vergara <javiera.vergara.s@usach.cl>
2023-11-20 12:48:04 -0300

    arreglo front

--- Frontend/pages/emergencia.vue
+++ Frontend/pages/emergencia.vue
@@ -71,10 +71,8 @@
                             Selecciona una opción
                           </option>
                           <option
-                            v-for="(objeto, indice) in regionesChile"
-                            :key="indice"
-                          >
-                            {{ objeto }}
+                            v-for="regionObj in regionesChile" :key="regionObj.valor">
+                            {{ regionObj.nombre }}
                           </option>
                         </select>
                       </td>
@@ -145,22 +143,22 @@
         selectedCoordinates: null,
         region: '',
         regionesChile: [
-          'Arica y Parinacota',
-          'Tarapacá',
-          'Antofagasta',
-          'Atacama',
-          'Coquimbo',
-          'Valparaíso',
-          'Metropolitana',
-          'O\'Higgins',
-          'Maule',
-          'Ñuble',
-          'Biobío',
-          'La Araucanía',
-          'Los Ríos',
-          'Los Lagos',
-          'Aysén',
-          'Magallanes'
+          {nombre: 'Arica y Parinacota', valor: 1},
+          {nombre: 'Tarapaca', valor: 2},
+          {nombre: 'Antofagasta', valor: 3},
+          {nombre: 'Atacama', valor: 4},
+          {nombre: 'Coquimbo', valor: 5},
+          {nombre: 'Valparaiso', valor: 6},
+          {nombre: 'Metropolitana', valor: 7},
+          {nombre: 'Ohiggins', valor: 8},
+          {nombre: 'Maule', valor: 9},
+          {nombre: 'Ñuble', valor: 10},
+          {nombre: 'Biobío', valor: 11},
+          {nombre: 'La Araucania', valor: 12},
+          {nombre: 'Los Rios', valor: 13},
+          {nombre: 'Los Lagos', valor: 14},
+          {nombre: 'Aysén', valor: 15},
+          {nombre: 'Magallanes', valor: 16},
         ],
 
         // Datos traidos desde el backend
@@ -223,39 +221,6 @@
           latitud: this.selectedCoordinates.lat,
           longitud: this.selectedCoordinates.lng,
           region: this.region
-        }
-        if (this.region === 'Metropolitana') {
-          emergencia.region = 'POLYGON((-70.896607 -32.933391,-71.709595 -33.969179,-70.720825 -33.919051,-69.808960 -34.242080,-69.781494 -33.338177,-70.896607 -32.933391))';
-        }else if(this.region === 'Arica y Parinacota'){
-          emergencia.region = 'POLYGON((-69.472047 -17.504372,-69.142457 -18.152809,-69.307252 -19.131327,-70.290528 -19.224717,-70.367432 -18.351047,-69.472047 -17.504372))';
-        }else if(this.region === 'Tarapaca'){
-          emergencia.region = 'POLYGON((-70.285035 -19.231632,-70.010377 -21.410455,-69.644166 -21.514830,-68.479616 -20.941345,-68.957521 -18.948286,-70.285035 -19.231632))';
-        }else if(this.region === 'Antofagasta'){
-          emergencia.region = 'POLYGON((-70.061647 -21.439006,-70.534059 -25.825850,-67.315065 -24.012596,-67.194215 -22.802767,-68.490602 -20.947330,-70.061647 -21.439006))';
-        }else if(this.region === 'Atacama'){
-          emergencia.region = 'POLYGON((-70.629274 -26.043209,-71.497194 -29.186934,-69.970094 -29.483837,-68.322145 -27.026099,-68.563844 -25.290644,-70.629274 -26.043209))';
-        }else if(this.region === 'Coquimbo'){
-          emergencia.region = 'POLYGON((-71.500856 -29.190131,-71.533815 -32.193819,-70.325319 -32.277456,-69.830934 -30.144729,-69.973757 -29.496587,-71.500856 -29.190131))';
-        }else if(this.region === 'Valparaiso'){
-          emergencia.region = 'POLYGON((-71.835937 -33.894357,-70.847168 -32.922249,-70.094604 -33.055867,-70.737304 -32,023213,-71.533813 -32.190722,-71.835937 -33.894357))';
-        }else if(this.region === 'Ohiggins'){
-          emergencia.region = 'POLYGON((-71.832275 -33.915633,-70.931396 -34.156894,-70.426025 -33.870035,-70.371093 -35.002628,-72.062988 -34.691568,-71.832275 -33.915633))';
-        }else if(this.region === 'Maule'){
-          emergencia.region = 'POLYGON((-72.107010 -34.841066,-70.423355 -35.039199,-70.448074 -35.887030,-70.957178 -36.377171,-72.694778 -35.953758,-72.107010 -34.841066))';
-        }else if(this.region === 'Ñuble'){
-          emergencia.region = 'POLYGON((-72.764470 -36.030953,-71.105968 -36.460293,-71.174220 -37.088978,-72.334488 -36.958200,-72.863151 -36.448587,-72.764470 -36.030953))';
-        }else if(this.region === 'Biobio'){
-          emergencia.region = 'POLYGON((-73.522058 -38.405718,-72.552892 -37.631458,-71.065018 -38.084104,-71.208345 -37.132521,-72.921448 -36.569999,-73.522058 -38.405718))';
-        }else if(this.region === 'La Araucania'){
-          emergencia.region = 'POLYGON((-73.201278 -39.367274,-71.508650 -39.583274,-73.501582 -38.491242,-72.552892 -37.658479,-71.071843 -38.110960,-73.201278 -39.367274))';
-        }else if(this.region === 'Los Rios'){
-          emergencia.region = 'POLYGON((-73.699511 -40.216820,-71.897681 -40.565104,-73.208103 -39.425291,-71.727054 -39.562230,-71.706578 -40.341786,-73.699511 -40.216820))';
-        }else if(this.region === 'Los Lagos'){
-          emergencia.region = 'POLYGON((-73.031082 -43.727752,-71.788912 -44.067097,-74.764228 -40.279332,-71.877206 -40.596206,-71.754354 -42.046701,-73.031082 -43.727752))';
-        }else if(this.region === 'Aysen'){
-          emergencia.region = 'POLYGON((-73.060564 -43.723173,-71.77181 -44.05938,-73.12523 -49.15964,-75.603293 -48.511322,-75.639227 -46.719030,-73.060564 -43.723173))';
-        }else if(this.region === 'Magallanes'){
-          emergencia.region = 'POLYGON((-73.184613 -49.173491,-66.442327 -55.136311,-67.208925 -55.882397,-73.465429 -54.081906,-75.616093 -48.861903,-73.184613 -49.173491))';
         }
         // Enviar emergencia
         try {
