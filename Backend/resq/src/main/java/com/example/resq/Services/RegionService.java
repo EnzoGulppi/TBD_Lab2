e2d7ac7393384364c551690c7231dfc68ae0e6e2
Javiera vergara <javiera.vergara.s@usach.cl>
2023-11-20 11:47:32 -0300

    Region listo

--- Backend/resq/src/main/java/com/example/resq/Services/RegionService.java
+++ Backend/resq/src/main/java/com/example/resq/Services/RegionService.java
@@ -1,10 +1,32 @@
 package com.example.resq.Services;
 
+import com.example.resq.Models.Region;
+import com.example.resq.Repository.RegionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/regiones")
 public class RegionService {
+    @Autowired
+    RegionRepository regionRepository;
+
+    @GetMapping
+    public List<Region> getAllRegiones(){return regionRepository.getAllRegiones();}
+
+    @GetMapping("/{id}")
+    public Region getRegionById(@PathVariable("id") Integer id){return regionRepository.getRegionById(id);}
+
+    @PostMapping
+    @ResponseBody
+    public void saveRegion(@RequestBody Region region){regionRepository.saveRegion(region);}
+
+    @DeleteMapping("/{id}")
+    public void deleteRegion(@PathVariable("id") Integer id){regionRepository.deleteRegion(id);}
+
+    @PutMapping
+    @ResponseBody
+    public Region updateRegion(@RequestBody Region region){return regionRepository.updateRegion(region);}
 }
